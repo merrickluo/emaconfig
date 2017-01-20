f1c1aa745021ac4d42c7b0407e030053ddb4385c
A.I <merrick@luois.me>
2017-01-20 11:39:42 +0800

    perfomace change

--- modules/global-settings.el
+++ modules/global-settings.el
@@ -1,5 +1,5 @@
 ;; -*- Emacs-Lisp -*-
-;; Last modified: <2017-01-19 10:04:06 Thursday by merrick>
+;; Last modified: <2017-01-20 11:38:00 Friday by merrick>
 
 ;; Copyright (C) 2017 A.I.
 
@@ -10,22 +10,30 @@
 ;; PUBLIC LICENSE: GPLv3
 
 ;;; Code:
+
+;; behavior settings
+(defalias 'yes-or-no-p 'y-or-n-p)
+(global-set-key (kbd "C-x k") 'kill-this-buffer)
+(setq inhibit-splash-screen t)
+(setq initial-scratch-message ";; time ticking
+")
+
+;; kill term buffer after exit
+(defadvice term-handle-exit
+  (after term-kill-buffer-on-exit activate)
+(kill-buffer))
+
+;; hlem
 (use-package helm
-	:ensure t
+	:defer t
   :bind (("M-x" . helm-M-x)
          ("C-x C-f" . helm-find-files)
          ("C-x C-b" . helm-buffers-list)
          ("C-x C-r" . helm-recentf)))
 
-(defalias 'yes-or-no-p 'y-or-n-p)
-(global-set-key (kbd "C-x k") 'kill-this-buffer)
-
-(setq inhibit-splash-screen t)
-(setq initial-scratch-message ";; time ticking
-")
-
+;; projectile
 (use-package projectile
-  :init
+	:init
 	(setq projectile-keymap-prefix (kbd "C-x p"))
 	:config
 	(def-projectile-commander-method ?t
@@ -34,15 +42,13 @@
 	(setq projectile-switch-project-action #'projectile-commander)
 	(projectile-global-mode))
 
-;; projectile
-(use-package helm-ag)
-
 (use-package helm-projectile
-	:after (projectile helm-ag)
+	:after (projectile)
 	:bind
 	(("<f1>" . helm-projectile-find-file)
 	 ("<f2>" . helm-projectile-ag))
 	:config
+	(use-package helm-ag)
 	(helm-projectile-on))
 
 (defun run-term()
@@ -51,10 +57,6 @@
 			(projectile-run-term shell-file-name)
 		(term shell-file-name)))
 
-;; kill term buffer after exit
-(defadvice term-handle-exit
-  (after term-kill-buffer-on-exit activate)
-(kill-buffer))
 
 (global-set-key (kbd "<f3>") 'run-term)
 
--- modules/go-settings.el
+++ modules/go-settings.el
@@ -1,8 +1,7 @@
 (use-package go-mode
 	:ensure t
-  :after (go-eldoc go-autocomplete)
-  :commands (gofmt gofmt-before-save)
-  :mode "\\.go\\'"
+  :after (go-eldoc company-go)
+	:mode "\\.go\\'"
 	:bind
 	(:map go-mode-map
 				("M-." . godef-jump)
@@ -13,20 +12,13 @@
 						(lambda ()
 							(set (make-local-variable 'company-backends) '(company-go)))))
 
-(use-package flycheck
-	:ensure t
-	:config
-	(add-hook 'go-mode-hook flycheck-mode))
-
-(use-package company-go
-	:ensure t
-	:defer t
-	:after (go-mode))
-
+(use-package company-go :defer t)
 (use-package go-eldoc
-	:ensure t
-  :init
+  :defer t
+  :config
   (add-hook 'go-mode-hook 'go-eldoc-setup))
+
+(use-package go-impl :defer t)
 
 (use-package protobuf-mode
 	:mode "\\.proto\\'")
