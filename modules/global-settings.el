1996ea594013113a56ea7514e7b0f06c50ce3ae5
A.I <merrick@luois.me>
2017-02-18 17:48:43 +0800

    try out ivy and better multi-term

--- modules/global-settings.el
+++ modules/global-settings.el
@@ -3,7 +3,7 @@
 ;; 
 ;; Author: A.I.
 ;; Email: merrick@luois.me
-;; Last modified: <2017-02-16 23:45:51 Thursday by merrick>
+;; Last modified: <2017-02-18 17:46:21 Saturday by merrick>
 ;; Copyright (C) 2017 A.I. all rights reserved.
 ;; PUBLIC LICENSE: GPLv3
 ;;
@@ -22,8 +22,8 @@
 ;; End:
 ;;
 ;;; Code:
-;; behavior settings
 
+;; behavior settings
 (defalias 'yes-or-no-p 'y-or-n-p)
 (global-set-key (kbd "C-x k") 'kill-this-buffer)
 (setq inhibit-splash-screen t)
@@ -36,12 +36,12 @@
 	(kill-buffer))
 
 ;; hlem
-(use-package helm
-	:defer t
-  :bind (("M-x" . helm-M-x)
-         ("C-x C-f" . helm-find-files)
-         ("C-x C-b" . helm-buffers-list)
-         ("C-x C-r" . helm-recentf)))
+;; (use-package helm
+;; 	:defer t
+;;   :bind (("M-x" . helm-M-x)
+;;          ("C-x C-f" . helm-find-files)
+;;          ("C-x C-b" . helm-buffers-list)
+;;          ("C-x C-r" . helm-resume)))
 
 ;; projectile
 (use-package projectile
@@ -51,38 +51,64 @@
 	(def-projectile-commander-method ?t
 		"Open Terminal in project root"
 		(run-term))
+	(setq projectile-completion-system 'ivy)
 	(setq projectile-switch-project-action #'projectile-commander)
 	(setq projectile-enable-caching t)
 	(projectile-global-mode))
 
-(use-package helm-projectile
-	:pin melpa
-	:after (projectile)
+(use-package swiper
+	:commands (swiper ivy-resume)
 	:bind
-	(("<f1>" . helm-projectile-find-file)
-	 ("<f2>" . helm-projectile-ag))
-	:config
-	(use-package helm-ag)
-	(helm-projectile-on))
+	(("C-s" . swiper)
+	 ("C-c C-r" . ivy-resume)))
 
-(defun projectile-run-multi-term (program)
-	"Run multi-term in project root, PROGRAM is shell to run."
-	(interactive)
-  (let* ((term (concat "multi-term " (projectile-project-name)))
-         (buffer (concat "*" term "*")))
-    (unless (get-buffer buffer)
-      (require 'term)
-      (let ((program (or program
-                         (read-from-minibuffer "Run program: "
-                                               (or explicit-shell-file-name
-                                                   (getenv "ESHELL")
-                                                   (getenv "SHELL")
-                                                   "/bin/sh")))))
-        (projectile-with-default-dir (projectile-project-root)
-          (set-buffer (make-term term program))
-          (term-mode)
-          (term-char-mode))))
-    (switch-to-buffer buffer)))
+(use-package counsel
+	:commands (counsel-M-x)
+	:bind
+	(("M-x" . counsel-M-x)
+	 ("C-x C-f" . counsel-find-file)
+	 ("C-h f" . counsel-describe-function)
+	 ("C-h v" . counsel-describe-variable)
+	 ("C-h i" . counsel-info-lookup-symbol)
+	 ([f1] . counsel-projectile-find-file)
+	 (:map read-expression-map
+				 ("C-r" . counsel-expression-history)))
+	)
+
+;; (use-package counsel-projectile
+;; 	:pin melpa)
+
+;; (use-package swiper
+;; 	:config
+;; 	(ivy-mode 1)
+;; 	(setq ivy-use-virtual-buffers t)
+;; 	;; (global-set-key "\C-s" 'swiper)
+;; 	;; (global-set-key (kbd "C-c C-r") 'ivy-resume)
+;; 	;; (global-set-key (kbd "<f6>") 'ivy-resume)
+;; 	;; (global-set-key (kbd "M-x") 'counsel-M-x)
+;; 	;; (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+;; 	;; (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+;; 	;; (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+;; 	;; (global-set-key (kbd "<f1> l") 'counsel-find-library)
+;; 	;; (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+;; 	;; (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+;; 	;; (global-set-key (kbd "C-c g") 'counsel-git)
+;; 	;; (global-set-key (kbd "C-c j") 'counsel-git-grep)
+;; 	;; (global-set-key (kbd "C-c k") 'counsel-ag)
+;; 	;; (global-set-key (kbd "C-x l") 'counsel-locate)
+;; 	;; (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+;; 	;; (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+;; 	)
+
+;; (use-package helm-projectile
+;; 	:pin melpa
+;; 	:after (projectile)
+;; 	:bind
+;; 	(("<f1>" . helm-projectile-find-file)
+;; 	 ("<f2>" . helm-projectile-ag))
+;; 	:config
+;; 	(use-package helm-ag)
+;; 	(helm-projectile-on))
 
 (defun run-term()
 	(interactive)
@@ -100,7 +126,30 @@
 	(yas-reload-all))
 
 (use-package multi-term
-	:pin melpa)
+	:pin melpa
+	:bind (([f3] . smart-run-term)
+				 ("C-<f3>" . dedicated-term))
+	:config
+	(defun dedicated-term()
+		(interactive)
+		(unless (multi-term-dedicated-exist-p)
+			(multi-term-dedicated-open))
+		(multi-term-dedicated-select))
+	(defun smart-run-term()
+    (interactive)
+		(if (projectile-project-p)
+				(let* ((term-name (concat "*term " (projectile-project-name) "*"))
+							 (buffer (get-buffer term-name)))
+          (if (not (eq buffer nil))
+							(switch-to-buffer buffer)
+						(progn
+							(multi-term)
+							(rename-buffer term-name)
+              ;; (comint-send-string (get-buffer-process term-name)
+							;; 										(format "cd %s\n" (projectile-project-root)
+							;; 														))
+							)))
+			(multi-term))))
 
 (use-package ace-window
 	:bind (("C-x o" . ace-window)))
