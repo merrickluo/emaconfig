6743a0f94977aaeee3f7592c1e9baf19f4386ea6
A.I <merrick@luois.me>
2017-02-20 16:37:20 +0800

    use c-n c-p in company popup

--- modules/company-settings.el
+++ modules/company-settings.el
@@ -3,7 +3,7 @@
 ;; 
 ;; Author: A.I.
 ;; Email: merrick@luois.me
-;; Last modified: <2017-02-18 18:13:46 Saturday by merrick>
+;; Last modified: <2017-02-20 16:36:31 Monday by merrick>
 ;; Copyright (C) 2017 A.I. all rights reserved.
 ;; PUBLIC LICENSE: GPLv3
 ;;
@@ -24,6 +24,10 @@
 
 (use-package company
 	:config
+	(define-key company-active-map (kbd "C-n") 'company-select-next)
+	(define-key company-active-map (kbd "C-p") 'company-select-previous)
+	(define-key company-search-map (kbd "C-n") 'company-select-next)
+	(define-key company-search-map (kbd "C-p") 'company-select-previous)
 	(global-company-mode)
 	(setq company-idle-delay 0)
 	(setq company-minimum-prefix-length 2))
