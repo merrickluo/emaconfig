7c6b18c9ae24eb431647e3d059beb96f5597d214
A.I <merrick@luois.me>
2017-02-21 17:23:53 +0800

    add some go package

--- modules/go-settings.el
+++ modules/go-settings.el
@@ -3,7 +3,7 @@
 ;; 
 ;; Author: A.I.
 ;; Email: merrick@luois.me
-;; Last modified: <2017-02-16 23:48:55 Thursday by merrick>
+;; Last modified: <2017-02-21 17:21:03 Tuesday by merrick>
 ;; Copyright (C) 2017 A.I. all rights reserved.
 ;; PUBLIC LICENSE: GPLv3
 ;;
@@ -31,16 +31,30 @@
 				("M-." . godef-jump)
 				("M-*" . pop-tag-mark))
   :config
-	(add-hook 'before-save-hook 'gofmt-before-save)
-	(use-package company-go
-		:config
-		(add-hook 'go-mode-hook
-							(lambda ()
-								(set (make-local-variable 'company-backends) '(company-go)))))
-	(use-package go-eldoc
-		:config
-		(add-hook 'go-mode-hook 'go-eldoc-setup))
-	(use-package go-impl))
+	(add-hook 'before-save-hook 'gofmt-before-save))
+
+(use-package company-go
+	:after go-mode
+	:config
+	(add-hook 'go-mode-hook
+						(lambda ()
+							(set (make-local-variable 'company-backends) '(company-go)))))
+
+(use-package go-impl
+	:after go-mode)
+
+(use-package go-add-tags
+	:after go-mode)
+
+(use-package go-eldoc
+	:after go-mode
+	:config
+	(add-hook 'go-mode-hook 'go-eldoc-setup))
+
+(use-package go-direx
+	:after go-mode
+	:bind (:map go-mode-map
+							("C-c C-j" . go-direx-pop-to-buffer)))
 
 (use-package protobuf-mode
 	:mode "\\.proto\\'")
