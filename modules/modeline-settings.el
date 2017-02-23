ca01dd1fe69ba42a85fb67c580d3b0c43177fb19
A.I <merrick@luois.me>
2017-02-23 18:14:15 +0800

    modeline version 0.1

--- modules/modeline-settings.el
+++ modules/modeline-settings.el
@@ -3,7 +3,7 @@
 ;; 
 ;; Author: A.I.
 ;; Email: merrick@luois.me
-;; Last modified: <2017-02-16 23:53:19 Thursday by merrick>
+;; Last modified: <2017-02-23 17:49:01 Thursday by merrick>
 ;; Copyright (C) 2017 A.I. all rights reserved.
 ;; PUBLIC LICENSE: GPLv3
 ;;
@@ -27,6 +27,18 @@
 ;; 	(display-time)
 ;; 	(setq sml/theme 'light-powerline)
 ;; 	(sml/setup))
+
+(use-package all-the-icons :pin melpa)
+
+(use-package spaceline-all-the-icons
+	:after spaceline
+	:ensure nil
+  :load-path "~/emaconfig/modules/spaceline-all-the-icons")
+
+(use-package spaceline
+	:after powerline
+  :config
+	(setq-default mode-line-format '("%e" (:eval (spaceline-ml-ati)))))
 
 (display-time)
 
