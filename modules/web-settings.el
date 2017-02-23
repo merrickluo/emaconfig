ab2abe10d29d1a8eb29a695948e00ef6e5b15a4a
A.I <merrick@luois.me>
2017-02-23 22:15:50 +0800

    add json mode

--- modules/web-settings.el
+++ modules/web-settings.el
@@ -3,7 +3,7 @@
 ;; 
 ;; Author: A.I.
 ;; Email: merrick@luois.me
-;; Last modified: <2017-02-21 21:55:39 Tuesday by merrick>
+;; Last modified: <2017-02-21 22:08:35 Tuesday by merrick>
 ;; Copyright (C) 2017 A.I. all rights reserved.
 ;; PUBLIC LICENSE: GPLv3
 ;;
@@ -20,10 +20,14 @@
 ;;; Code:
 
 (use-package js2-mode
-	:mode (("\\.js\\'" . js2-mode)
-				 ("\\.json\\'" . json-mode))
+	:commands (js2-mode)
+	:mode (("\\.js\\'" . js2-mode))
   :config
 	(setq js2-basic-offset 2))
+
+(use-package json-mode
+	:commands (json-mode)
+	:mode "\\.json\\'")
 
 (provide 'web-settings)
 ;;; web-settings.el ends here
