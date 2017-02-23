9b4c6e321721c6c915b50e2322183e407bfc8ab7
A.I <merrick@luois.me>
2017-02-23 11:08:36 +0800

    let comany-mode keep case

--- modules/company-settings.el
+++ modules/company-settings.el
@@ -3,7 +3,7 @@
 ;; 
 ;; Author: A.I.
 ;; Email: merrick@luois.me
-;; Last modified: <2017-02-20 16:36:31 Monday by merrick>
+;; Last modified: <2017-02-22 10:39:42 Wednesday by merrick>
 ;; Copyright (C) 2017 A.I. all rights reserved.
 ;; PUBLIC LICENSE: GPLv3
 ;;
@@ -30,6 +30,7 @@
 	(define-key company-search-map (kbd "C-p") 'company-select-previous)
 	(global-company-mode)
 	(setq company-idle-delay 0)
+	(setq company-dabbrev-downcase nil)
 	(setq company-minimum-prefix-length 2))
 
 ;; (use-package company-flx
