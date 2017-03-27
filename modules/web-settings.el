2389930e238b935f833aff58a8323c1e0349bc31
A.I <merrick@luois.me>
2017-03-28 00:10:46 +0800

    conditional use web-mode or js-mode in js file

--- modules/web-settings.el
+++ modules/web-settings.el
@@ -3,7 +3,7 @@
 ;;
 ;; Author: A.I.
 ;; Email: merrick@luois.me
-;; Last modified: <2017-03-27 20:40:33 Monday by merrick>
+;; Last modified: <2017-03-28 00:10:23 Tuesday by merrick>
 ;; Copyright (C) 2017 A.I. all rights reserved.
 ;; PUBLIC LICENSE: GPLv3
 ;;
@@ -63,12 +63,21 @@
 	(add-to-list 'company-backends 'company-tern)
 	(setq company-tern-property-marker " <p>"))
 
+(defun react-js-mode ()
+	(if (equal (getenv "REACT") "1")
+      (web-mode)
+		(js-mode)))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . react-js-mode))
+
 (use-package web-mode
   :mode "\\.jsx\\'"
+	:commands web-mode
 	:config
 	(setq web-mode-markup-indent-offset 2
         web-mode-css-indent-offset 2
         web-mode-code-indent-offset 2)
+	(setq web-mode-content-types-alist
+				'(("jsx" . "\\.js[x]?\\'")))
   (setq js-indent-level 2)
 	(use-package flycheck
 		:config
