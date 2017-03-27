e404690ff7dd773c1aea2d5f62bd66be602fdaf4
A.I <merrick@luois.me>
2017-03-27 16:59:47 +0800

    better modeline setup

--- modules/modeline-settings.el
+++ modules/modeline-settings.el
@@ -1,20 +1,20 @@
 ;;; modeline-settings.el --- summary
 ;; Description:
-;; 
+;;
 ;; Author: A.I.
 ;; Email: merrick@luois.me
-;; Last modified: <2017-02-28 10:21:27 Tuesday by merrick>
+;; Last modified: <2017-03-27 16:55:22 Monday by merrick>
 ;; Copyright (C) 2017 A.I. all rights reserved.
 ;; PUBLIC LICENSE: GPLv3
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Commentary:
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Change log:
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; Code:
@@ -28,16 +28,22 @@
 ;; 	(setq sml/theme 'light-powerline)
 ;; 	(sml/setup))
 
-(use-package all-the-icons :pin melpa)
+(use-package all-the-icons)
 
-(use-package spaceline-all-the-icons
-	:after spaceline
-	:ensure nil
-  :load-path "~/emaconfig/modules/spaceline-all-the-icons"
+(use-package spaceline
+	:demand t
 	:config
+	(load-file (concat emacs-root-path "/modules/spaceline-all-the-icons.el"))
 	(setq-default mode-line-format '("%e" (:eval (spaceline-ml-ati)))))
 
-(use-package spaceline)
+;; (use-package spaceline-all-the-icons
+;; 	:init
+;; 	(use-package spaceline :demand t)
+;; 	:ensure nil
+;;   :load-path "~/emaconfig/modules/spaceline-all-the-icons"
+;; 	:config
+;; 	(setq-default mode-line-format '("%e" (:eval (spaceline-ml-ati)))))
+
 
 (provide 'modeline-settings)
 ;;; modeline-settings.el ends here
