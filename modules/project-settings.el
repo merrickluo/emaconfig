a02640ceb9d07556d9c41622ea9e7f910a2ef2d7
A.I <merrick@luois.me>
2017-08-29 16:17:36 +0800

    org-projectile breaking change

--- modules/project-settings.el
+++ modules/project-settings.el
@@ -3,7 +3,7 @@
 ;;
 ;; Author: A.I.
 ;; Email: merrick@luois.me
-;; Last modified: <2017-06-24 11:02:27 Saturday by merrick>
+;; Last modified: <2017-08-29 14:54:55 Tuesday by merrick>
 ;; Copyright (C) 2017 A.I. all rights reserved.
 ;; PUBLIC LICENSE: GPLv3
 ;;
@@ -37,17 +37,19 @@
       (setq line (string-remove-prefix c line)))
     (comment-string-strip line t t)))
 
+
+
 ;; TODO hello world
 (use-package org-projectile
 	:demand t
   :bind (("C-c n p" . org-projectile:project-todo-completing-read)
          ("C-c c" . org-capture))
   :config
-	(org-projectile:per-repo)
-	(setq org-projectile:per-repo-filename "todos.org")
-	(setq org-agenda-files (append org-agenda-files (org-projectile:todo-files)))
-	(setq project-todo-template "* %(capture-todo-comment \"%i\") %?\n  %l")
-	(add-to-list 'org-capture-templates (org-projectile:project-todo-entry "p" project-todo-template)))
+	(org-projectile-per-project)
+	(setq org-projectile-per-project-filepath "my_project_todo_filename.org")
+	(setq org-agenda-files (append org-agenda-files (org-projectile-todo-files)))
+	(global-set-key (kbd "C-c c") 'org-capture)
+	(global-set-key (kbd "C-c n p") 'org-projectile-project-todo-completing-read))
 
 (provide 'project-settings)
 ;;; project-settings.el ends here
