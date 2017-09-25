5a87e94a8375ce79c08912e6f766e0272f9704a8
A.I <merrick@luois.me>
2017-09-25 21:49:39 +0800

    windows related settings

--- modules/windows-settings.el
+++ modules/windows-settings.el
@@ -3,7 +3,7 @@
 ;;
 ;; Author: A.I.
 ;; Email: merrick@luois.me
-;; Last modified: <2017-09-01 15:27:17 Friday by Merrick>
+;; Last modified: <2017-09-07 17:03:05 Thursday by Merrick>
 ;; Copyright (C) 2017 A.I. all rights reserved.
 ;; PUBLIC LICENSE: GPLv3
 ;;
@@ -24,6 +24,12 @@
 (setq inhibit-compacting-font-caches t)
 (setq counsel-ag-base-command "ag --nocolor --nogroup --vimgrep %s")
 (setq-default buffer-file-coding-system 'utf-8-unix)
+
+(when (boundp 'w32-pipe-read-delay)
+  (setq w32-pipe-read-delay 0))
+;; Set the buffer size to 64K on Windows (from the original 4K)
+(when (boundp 'w32-pipe-buffer-size)
+  (setq irony-server-w32-pipe-buffer-size (* 64 1024)))
 
 (provide 'windows-settings)
 ;;; windows-settings.el ends here
