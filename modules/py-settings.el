98ab5851081ffaca8e5f3e62d7329e1728da3c34
A.I <merrick@luois.me>
2017-11-14 21:22:04 +0800

    py settings

--- modules/py-settings.el
+++ modules/py-settings.el
@@ -1,21 +1,23 @@
 (use-package anaconda-mode
 	:config
-	(use-package company-anaconda)
 	(add-hook 'python-mode-hook 'anaconda-mode)
 	(add-hook 'python-mode-hook 'anaconda-eldoc-mode))
+
+(use-package company-anaconda
+	:config
+	(add-to-list 'company-backends 'company-anaconda))
 
 (use-package pyenv-mode
 	:config
 	(defun projectile-pyenv-mode-set ()
+		(interactive)
 		"Set pyenv version matching project name."
 		(let ((project (projectile-project-name)))
 			(if (member project (pyenv-mode-versions))
 					(pyenv-mode-set project)
 				(pyenv-mode-unset))))
-
+	(add-hook 'persp-switch-hook 'projectile-pyenv-mode-set)
 	(add-hook 'projectile-switch-project-hook 'projectile-pyenv-mode-set))
-
-
 
 (provide 'py-settings)
 ;;; py-settings.el ends here
