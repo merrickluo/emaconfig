a6a096ca01bdc8262b37614823b2b92cd61c2152
A.I <merrick@luois.me>
2017-12-26 13:55:11 +0800

    disable broken counsel-projectile

--- modules/global-settings.el
+++ modules/global-settings.el
@@ -97,46 +97,14 @@
 	 (:map read-expression-map
 				 ("C-r" . counsel-expression-history))))
 
-(use-package counsel-projectile
-	:pin melpa
-	:config
-	(ivy-set-display-transformer 'counsel-projectile
-															 'ivy-rich-switch-buffer-transformer)
-	(ivy-set-display-transformer 'counsel-projectile-switch-to-buffer
-															 'ivy-rich-switch-buffer-transformer)
-	(counsel-projectile-on))
-
-;; (use-package swiper
-;; 	:config
-;; 	(ivy-mode 1)
-;; 	(setq ivy-use-virtual-buffers t)
-;; 	;; (global-set-key "\C-s" 'swiper)
-;; 	;; (global-set-key (kbd "C-c C-r") 'ivy-resume)
-;; 	;; (global-set-key (kbd "<f6>") 'ivy-resume)
-;; 	;; (global-set-key (kbd "M-x") 'counsel-M-x)
-;; 	;; (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-;; 	;; (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-;; 	;; (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-;; 	;; (global-set-key (kbd "<f1> l") 'counsel-find-library)
-;; 	;; (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-;; 	;; (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-;; 	;; (global-set-key (kbd "C-c g") 'counsel-git)
-;; 	;; (global-set-key (kbd "C-c j") 'counsel-git-grep)
-;; 	;; (global-set-key (kbd "C-c k") 'counsel-ag)
-;; 	;; (global-set-key (kbd "C-x l") 'counsel-locate)
-;; 	;; (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-;; 	;; (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-;; 	)
-
-;; (use-package helm-projectile
+;; (use-package counsel-projectile
 ;; 	:pin melpa
-;; 	:after (projectile)
-;; 	:bind
-;; 	(("<f1>" . helm-projectile-find-file)
-;; 	 ("<f2>" . helm-projectile-ag))
 ;; 	:config
-;; 	(use-package helm-ag)
-;; 	(helm-projectile-on))
+;; 	(ivy-set-display-transformer 'counsel-projectile
+;; 															 'ivy-rich-switch-buffer-transformer)
+;; 	(ivy-set-display-transformer 'counsel-projectile-switch-to-buffer
+;; 															 'ivy-rich-switch-buffer-transformer)
+;; 	(counsel-projectile-mode))
 
 (use-package yasnippet
 	:commands (yas-minor-mode)
