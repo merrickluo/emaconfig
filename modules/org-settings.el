3ba22ffe93eaa28b957a8b8165239c76b089204b
A.I <merrick@luois.me>
2018-01-05 13:56:01 +0800

    add insert-bangumi

--- modules/org-settings.el
+++ modules/org-settings.el
@@ -2,10 +2,20 @@
   :ensure org-plus-contrib
 	:pin org
 	:config
+
+	(defun org-insert-bangumi ()
+		(interactive)
+		(save-excursion
+			(let ((name (read-string "Bangumi Name: "))
+						(length (read-number "Bangumi length: ")))
+				(insert "** " name " [/] \n")
+				(dotimes (index length)
+					(insert (format "\t- [ ] Episode %d \n" (+ index 1))))))
+		(call-interactively 'org-update-statistics-cookies))
+
 	(add-hook 'org-mode-hook 'yas-minor-mode)
 	(setq org-directory "~/Dropbox/docs")
 	(setq org-agenda-files '("~/Dropbox/docs"))
 	:bind (("C-c a" . org-agenda)))
-
 (provide 'org-settings)
 ;;; org-settings.el ends here
