8e34995cbfd096402393f1246d5b16bf211bd82a
A.I <merrick@luois.me>
2018-02-06 10:39:54 +0800

    use .python-version file for pyenv-mode-set

--- modules/py-settings.el
+++ modules/py-settings.el
@@ -1,5 +1,6 @@
 (use-package anaconda-mode
 	:config
+	(setq python-shell-interpreter "python")
 	(add-hook 'python-mode-hook 'anaconda-mode)
 	(add-hook 'python-mode-hook 'anaconda-eldoc-mode))
 
@@ -7,14 +8,24 @@
 	:config
 	(add-to-list 'company-backends 'company-anaconda))
 
+(defun get-string-from-file (filePath)
+  "Return filePath's file content."
+	(require 'subr-x)
+  (with-temp-buffer
+    (insert-file-contents filePath)
+    (string-trim (buffer-string))))
+
 (use-package pyenv-mode
 	:config
 	(defun projectile-pyenv-mode-set ()
 		(interactive)
 		"Set pyenv version matching project name."
-		(let ((project (projectile-project-name)))
-			(if (member project (pyenv-mode-versions))
-					(pyenv-mode-set project)
+		(let ((python-version-file (concat (projectile-project-root) ".python-version")))
+			(if (file-exists-p python-version-file)
+          (let ((python-version (get-string-from-file python-version-file)))
+						(if (member python-version (pyenv-mode-versions))
+								(pyenv-mode-set python-version)
+							(pyenv-mode-unset)))
 				(pyenv-mode-unset))))
 	(add-hook 'persp-switch-hook 'projectile-pyenv-mode-set)
 	(add-hook 'projectile-switch-project-hook 'projectile-pyenv-mode-set))
