f916455f7f9d97ed1db5955e256641964b63067a
A.I <merrick@luois.me>
2018-03-16 01:26:28 +0800

    more elfeed

--- modules/feed-settings.el
+++ modules/feed-settings.el
@@ -1,7 +1,13 @@
-(use-package elfeed)
+(use-package elfeed
+	:bind (("C-c f" . elfeed)))
 
 (use-package elfeed-org
 	:after elfeed
 	:config
 	(setq rmh-elfeed-org-files '("~/Dropbox/docs/feeds.org"))
 	(elfeed-org))
+
+(use-package elfeed-goodies
+	:after elfeed
+	:config
+	(elfeed-goodies/setup))
